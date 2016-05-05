6039302de1373eb341c21e8715c96680d2f81f24
James Loucks <etskh@hotmail.com>
2016-05-04 23:29:11 -0700

    Dog scores points! Wow! :dog:

--- Assets/Dogs/Dag.cs
+++ Assets/Dogs/Dag.cs
@@ -1,7 +1,101 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Dag : MonoBehaviour {
+public class Dag : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        _score = 0.0f;
+        _scoreMultiplier = 0.0f;
+        _multiplierIncrement = 1f;
+
+        _timeBetweenBorks = 1.0f;
+        _timeSinceLastBork = 0.0f;
+
+        _nextBounceSpeed = 5.0f;
+        _bounceIncrement = 1.0f;
+        _horizontalImpulse = 5.0f;
+        _hadCollision = false;
+
+        _timeSinceHit = float.MaxValue;
+        _timeToHit = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizImpulse = Time.deltaTime * _horizontalImpulse;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity -= new Vector2(horizImpulse, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(horizImpulse, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (_timeSinceLastBork > _timeBetweenBorks)
+            {
+                _timeSinceLastBork = 0;
+                Vector3 pos = this.GetComponentInChildren<Transform>().position;
+
+                //GameObject bork = GameObject.Instantiate(borkPrototype);
+                //bork.transform.position = pos;
+
+                addScore(100);
+            }
+        }
+        _timeSinceLastBork += Time.deltaTime;
+
+        //
+        // Hit the jump right before landing to increase the multiplier
+        //
+        if( Input.GetButtonDown("Vertical"))
+        {
+            _timeSinceHit = 0.0f;
+        }
+        _timeSinceHit += Time.deltaTime;
+
+        //
+        // Follow the dog
+        //
+        Vector3 cameraPosition = Camera.main.transform.position;
+        float lockSpeed = _nextBounceSpeed / 5.0f;
+        cameraPosition = Vector3.Lerp(cameraPosition, transform.position, Time.deltaTime * lockSpeed);
+        cameraPosition.z = -5; // always make it -5
+        Camera.main.transform.position = cameraPosition;
+
+        //
+        // Zoom out as he goes higher
+        //
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 4 + Mathf.Log10(_nextBounceSpeed), Time.deltaTime);
+
+
+        //
+        // Update the score
+        //
+        GameObject.Find("Score").GetComponent<TextMesh>().text = _score.ToString() + "\n<size=48>x</size> " + _scoreMultiplier.ToString();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Trampoline")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, _nextBounceSpeed);
+            if ( _timeSinceHit < _timeToHit )
+            {
+                incrementMultiplier();
+                _nextBounceSpeed += _bounceIncrement;
+            }
+            _hadCollision = false;
+        }
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            _hadCollision = true;
+        }
+    }
 
     public void resetMultiplier()
     {
@@ -10,105 +104,38 @@ public class Dag : MonoBehaviour {
     public void incrementMultiplier()
     {
         _scoreMultiplier += _multiplierIncrement;
+        EmitText("<size=48>x</size> " + _scoreMultiplier.ToString(), transform.position + new Vector3(1, -1));
     }
     public void addScore(float score)
     {
-        _score += score * (1.0f + _scoreMultiplier);
-        EmitText(score.ToString());
+        float multipliedScore = score * _scoreMultiplier ;
+        _score += multipliedScore;
+        EmitText(multipliedScore.ToString(), transform.position + new Vector3(1,1));
     }
 
-    public void EmitText( string str )
+    public void EmitText(string str, Vector3 pos )
     {
+        pos.z -= 0.2f;
         GameObject textInstance = Instantiate(textPrototype);
-        //textInstance.transform.position = transform.position;
-        //textInstance.GetComponent<Text>().Text = text;
+        textInstance.transform.position = pos;
+        textInstance.GetComponent<TextMesh>().text = str;
     }
 
-    // Use this for initialization
-    void Start () {
-        _score = 0;
-        _scoreMultiplier = 0;
-        _multiplierIncrement = 0.1f;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        float horizImpulse = Time.deltaTime * horizontalImpulse;
-	    if( Input.GetKey(KeyCode.LeftArrow) )
-        {
-            GetComponent<Rigidbody2D>().velocity -= new Vector2(horizImpulse,0);
-        }
-        if(Input.GetKey(KeyCode.RightArrow) )
-        {
-            GetComponent<Rigidbody2D>().velocity += new Vector2(horizImpulse,0);
-        }
-
-        if( Input.GetKeyDown(KeyCode.Space) )
-        {
-            if(timeSinceLastBork > timeBetweenBorks )
-            {
-                timeSinceLastBork = 0;
-                Debug.Log("Bork!");
-                Vector3 pos = this.GetComponentInChildren<Transform>().position;
-                GameObject bork = GameObject.Instantiate(borkPrototype);
-                bork.transform.position = pos;
-            }
-        }
-        timeSinceLastBork += Time.deltaTime;
-
-        //
-        // Follow the dog
-        //
-        Vector3 cameraPosition = Camera.main.transform.position;
-        float lockSpeed = nextBounceSpeed / 5.0f;
-        cameraPosition = Vector3.Lerp(cameraPosition, transform.position, Time.deltaTime * lockSpeed);
-        cameraPosition.z = -5; // always make it -5
-        Camera.main.transform.position = cameraPosition;
-        //
-        // Zoom out as he goes higher
-        //
-        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 4 + Mathf.Log10(nextBounceSpeed), Time.deltaTime); 
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if( collision.gameObject.name == "Trampoline" )
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, nextBounceSpeed);
-            if (!hadCollision)
-            {
-                nextBounceSpeed += bounceIncrement;
-            }
-            hadCollision = false;
-        }
-        if (collision.gameObject.CompareTag("Obstacle") )
-        {
-            hadCollision = true;
-        }
-    }
-
-    
-
-    // 
     private float _score;
     private float _scoreMultiplier;
     private float _multiplierIncrement;
+
     public GameObject textPrototype;
-
-    //TODO: _ify
-    //UNUSED private float cameraFollowSpeed = 3;
-
     public GameObject borkPrototype;
-    //TODO: _ify
-    private float timeBetweenBorks = 3;
-    //TODO: _ify
-    private float timeSinceLastBork = 0;
 
-    public float nextBounceSpeed = 5;
-    //TODO: _ify
-    private float bounceIncrement = 1;
-    //TODO: _ify
-    private float horizontalImpulse = 5;
-    //TODO: _ify
-    private bool hadCollision = false;
+    private float _timeBetweenBorks;
+    private float _timeSinceLastBork;
+
+    public float _nextBounceSpeed;
+    private float _bounceIncrement;
+    private float _horizontalImpulse;
+    private bool _hadCollision;
+
+    private float _timeToHit;
+    private float _timeSinceHit;
 }
---
+++ Assets/UI/ScoreText.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        _upSpeed = 1.0f;
+        _scaleSpeed = 0.5f;
+        _timeTillDeath = 1.0f;
+        _aliveTime = 0.0f;
+
+        _fadeOutColour = new Color(1, 1, 1, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float deltaScale = _scaleSpeed * Time.deltaTime;
+        transform.position += new Vector3(0.0f, _upSpeed * Time.deltaTime, 0.0f);
+        transform.localScale += new Vector3( deltaScale, deltaScale, deltaScale);
+
+        TextMesh tm = GetComponent<TextMesh>();
+        tm.color = Color.Lerp(tm.color, _fadeOutColour, Time.deltaTime * _timeTillDeath);
+        _aliveTime += Time.deltaTime;
+
+        if (_aliveTime > _timeTillDeath )
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private float _upSpeed;
+    private float _scaleSpeed;
+    private float _timeTillDeath;
+    private float _aliveTime;
+    private Color _fadeOutColour;
+}
